689585a51ea2d5cce2a6e4d2b950b0b7d6d2636a
Nguyễn Duy Tân <tantuoiti2030@gmail.com>
2020-05-09 11:13:35 +0700

    diem danh mon PTTKHT

---
+++ Client_sim/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_sim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient tcpClient = new TcpClient();
+            tcpClient.Connect("192.168.1.69", 5000);
+            Console.WriteLine("connect successfull");
+            Stream stream = tcpClient.GetStream();
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] data = encoding.GetBytes(Dns.GetHostByName(Dns.GetHostName()).AddressList[0].ToString());
+            stream.Write(data, 0, data.Length);
+            data = new byte[1024];
+            stream.Read(data, 0, 1024);
+            Console.WriteLine(encoding.GetString(data));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sever_sim/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sever_sim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("192.168.1.69");
+            TcpListener listener = new TcpListener(ip, 5000);
+            listener.Start();
+            Console.WriteLine("Sever start on " + listener.LocalEndpoint);
+            Console.WriteLine("Waiting connect...");
+            while (true)
+            {
+                Socket socket = listener.AcceptSocket();
+                Console.WriteLine(socket.RemoteEndPoint + "conected");
+                byte[] data = new byte[1024];
+                socket.Receive(data);
+                ASCIIEncoding encoding = new ASCIIEncoding();
+                string str = encoding.GetString(data);
+                socket.Send(encoding.GetBytes("Hello from sever" + str));
+            }
+        }
+    }
+}
